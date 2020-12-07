fea1696a851c48a4c4bbb53cb7c6c43d5cb5439c
pavel.uxd <paveluxd@gmail.com>
2020-12-07 11:09:48 +0100

    1

--- index.js
+++ index.js
@@ -7,22 +7,41 @@ var app = express()
 //Listen to port number
 // process.env.PORT -> herokus var for port
 var server = app.listen(process.env.PORT || 4000, function(){
-    console.log('XDXD PORT 4000')
+    console.log('PORT 4000')
 })
 
 //Static files
 app.use(express.static('public'))
 
+var players = []
 
-// Socket server
+// SERVER SIDE SOCKET
 // Specifies the server you want to work with, in this case our express server
 // Socket param will be passed by client side, there will be a var per connection
 var io = socket(server)
 io.on('connection', function(socket){
+    //Connect
     console.log('Connected', socket.id)
 
-    //Listend for the event from client side sockets
+    players.push(socket.id)
+    io.sockets.emit('playerJoined', players)
+        
+    //Emit board
     socket.on('emitBoard', function(data){
         io.sockets.emit('emitBoard', data)
+        console.log('Board sent')
+    })
+
+    //Disconnect
+    socket.on('disconnect', function() {
+        const index = players.indexOf(socket.id);
+        if (index > -1) {
+            players.splice(index, 1);
+        }
+
+        io.sockets.emit('playerLeft', players)
+        console.log('Got disconnect!', socket.id); 
     })
 }) 
+
+
--- public/game.js
+++ public/game.js
@@ -1,10 +1,7 @@
 // SOCKET SETUP
-// Make connection
-
-// const { emit } = require("nodemon")
-
-// Frontend socket var
+// Make connection > Frontend socket var > Swap before uploading to heroku
 var socket = io.connect('https://arch-chess.herokuapp.com/' || 'http://localhost:4000')
+// var socket = io.connect('http://localhost:4000')
 
 // Sends data to the server
 function emitBoard(boardString){
@@ -17,6 +14,29 @@ socket.on('emitBoard', function(data){
     genBoardFromString(data)
 })
 
+// Player mangement
+players = []
+
+socket.on('playerJoined', function(data){    
+    players = data
+    console.log(data)
+    updatePlayerList()
+})
+
+socket.on('playerLeft', function(data){
+    players = data
+
+    updatePlayerList()
+})
+
+function updatePlayerList(){
+    document.getElementById('players').innerHTML = ''
+
+    for(i=0; i < players.length; i++){
+        document.getElementById('players').innerHTML += players[i] + ', '
+    }
+}
+
 
 // UTILITY
 // Set pointer to follow mouse
@@ -27,52 +47,7 @@ const onMouseMove = (e) =>{
 }
 document.addEventListener('mousemove', onMouseMove);
 
-// Game
-// Create a board
-function genBoard(){
-
-    var rows = ["row8","row7","row6","row5","row4","row3","row2","row1"]
-    var columns = ["colA","colB","colC","colD","colE","colF","colG","colH"]
-    var rowId, cell = null
-    for( i=0; i < 8; i++){
-        rowId = rows[i]
-
-        for( b=0; b < columns.length; b++){
-            cell = document.createElement("div")
-
-            cell.setAttribute("style", `grid-area: ${rowId}/${columns[b]};`)
-            cell.id=`${rowId}${columns[b]}`
-            cell.classList="blank"
-            cell.setAttribute("onclick", "select(this)")
-            // cell.innerHTML = `${columns[b].split('')[3]}${rowId.split('')[3]}`
-
-            document.querySelector(".board").appendChild(cell)
-        }
-    }  
-}
-
-genBoard()
-
-// Manipulate the board
-var targetElem = null
-var previousCell = null
-var storedFigure = null
-
-//STOR FIGURE
-function storeClass(elem){
-    storedFigure = document.getElementById(elem.id).classList[0]
-
-    //Remove selection
-    if(previousCell !== null){
-        previousCell.classList.remove('selected') 
-    }
-    previousCell = null
-
-    //Set pointer indicator
-    document.getElementById('pointer').classList = storedFigure
-}
-
-//NOTIFICATIONS
+// Notifications
 function notify(obj, method){
     var status = document.getElementById('status')
 
@@ -86,60 +61,8 @@ function notify(obj, method){
     }
 }
 
-// MAIN
-function select(elem) {
-    var selectedCell = document.getElementById(elem.id)
-
-    //Clear pointer
-    document.getElementById('pointer').classList = ''
-    //Clear status
-    document.getElementById('status').innerHTML = ''
-
-    // Place figure
-    if(storedFigure !== null){
-        document.getElementById(elem.id).classList = storedFigure
-
-        // notify(storedFigure, 'paste')
-        storedFigure = null
-
-        if(previousCell !== selectedCell){
-            previousCell.classList = 'blank'
-        }
-
-        //Set pointer indicator
-        document.getElementById('pointer').classList = 'blank'
-
-        // Store board state as a string
-        convertBoardToString()
-    }
-
-    // Store figure
-    else if(selectedCell.classList[0] !== 'blank'){
-        storedFigure = document.getElementById(elem.id).classList[0]
-        selectedCell.classList = 'blank'
-
-        //Set pointer indicator
-        document.getElementById('pointer').classList = storedFigure
-
-        // notify(storedFigure, 'save')
-    }
-
-    // Clear selection from previous cell
-    if(previousCell !== null){
-        previousCell.classList.remove('selected') 
-    }
-
-    // Overwrite source element
-    previousCell = document.getElementById(elem.id)
-    
-    // New selection
-    document.getElementById(elem.id).classList.add("selected"); 
-
-}
-
 // Convert board state to string
 var boardStateString
-let testSet = 'blank blank blank blank blank blank blank blank blank blank blank blank blank blank blank blank blank blank rook blank rook blank rook blank blank blank blank rook blank rook blank blank blank blank rook blank rook blank rook blank blank blank blank blank blank blank blank blank blank blank blank blank blank blank blank blank blank blank blank blank blank blank blank blank '
 
 function convertBoardToString(){
 
@@ -168,5 +91,98 @@ function genBoardFromString(string){
     for( i = 0; i < boardObj.length ; i++ ){
         boardObj[i].classList = boardFigureClasses[i]
     }
+
+}
+
+// GAME
+// Create a board
+function genBoard(){
+
+    var rows = ["row8","row7","row6","row5","row4","row3","row2","row1"]
+    var columns = ["colA","colB","colC","colD","colE","colF","colG","colH"]
+    var rowId, cell = null
+    for( i=0; i < 8; i++){
+        rowId = rows[i]
+
+        for( b=0; b < columns.length; b++){
+            cell = document.createElement("div")
+
+            cell.setAttribute("style", `grid-area: ${rowId}/${columns[b]};`)
+            cell.id=`${rowId}${columns[b]}`
+            cell.classList="blank"
+            cell.setAttribute("onclick", "select(this)")
+            // cell.innerHTML = `${columns[b].split('')[3]}${rowId.split('')[3]}`
+
+            document.querySelector(".board").appendChild(cell)
+        }
+    }  
+}
+genBoard()
+
+// Manipulate the board
+var targetElem = null
+var previousCell = null
+var storedFigure = null
+
+// Store figure
+function storeClass(elem){
+    storedFigure = document.getElementById(elem.id).classList[0]
+
+    //Remove selection
+    if(previousCell !== null){
+        previousCell.classList.remove('selected') 
+    }
+    previousCell = null
+
+    //Set pointer indicator
+    document.getElementById('pointer').classList = storedFigure
+}
+
+// Move
+function select(elem) {
+    var selectedCell = document.getElementById(elem.id)
+
+    //Clear pointer
+    document.getElementById('pointer').classList = ''
+    //Clear status
+    document.getElementById('status').innerHTML = ''
+
+    // Place figure
+    if(storedFigure !== null){
+        document.getElementById(elem.id).classList = storedFigure
+
+        // notify(storedFigure, 'paste')
+        storedFigure = null
+
+        if(previousCell !== selectedCell && previousCell !== null){
+            previousCell.classList = 'blank'
+        }
+
+        //Set pointer indicator
+        document.getElementById('pointer').classList = 'blank'
+
+        // Store board state as a string
+        convertBoardToString()
+    }
+
+    // Store figure
+    else if(selectedCell.classList[0] !== 'blank'){
+        storedFigure = document.getElementById(elem.id).classList[0]
+        selectedCell.classList = 'blank'
+
+        //Set pointer indicator
+        document.getElementById('pointer').classList = storedFigure
+    }
+
+    // Clear selection from previous cell
+    if(previousCell !== null){
+        previousCell.classList.remove('selected') 
+    }
+
+    // Overwrite source element
+    previousCell = document.getElementById(elem.id)
+    
+    // New selection
+    document.getElementById(elem.id).classList.add("selected"); 
 
 }
